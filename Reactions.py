d5d1c6afdbfe5ffa1800b8ab7ddcbfa88a15da97
Kyle Mok <kylemok@Kyles-MacBook-Pro.local>
2015-08-21 00:47:37 -0500

    added vector that stores and averages

--- Reactions.py
+++ Reactions.py
@@ -5,7 +5,7 @@ import random
 
 print "Let's measure your reaction time."
 print "Do you want to play?"
-
+vec = []
 
 while True:
     global play
@@ -29,6 +29,7 @@ while play == 'y':
     reaction = raw_input(">")
     end = time.time()
     speed = end - start
+    vec.append(speed)
     print "Your reaction time is ", speed, " seconds."
     print "Do you want to play again?"
     again = raw_input("y/n? :")
@@ -37,7 +38,9 @@ while play == 'y':
     else:
         print "Thanks for playing"
         break
- 
+
+
+print "Your average reaction time is ", sum(vec)/len(vec), " seconds."
 
 time.sleep(2)
        
